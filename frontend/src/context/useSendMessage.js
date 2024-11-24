7646c77ba42578479e1ab9df8a1404e6d4a72b3d
K K Harjeeth <107640043+KK-Harjeeth@users.noreply.github.com>
2024-11-24 20:45:25 +0530

    Update useSendMessage.js

--- frontend/src/context/useSendMessage.js
+++ frontend/src/context/useSendMessage.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 import useConversation from "../stateManage/useConversation.js";
 import axios from "axios";
+import Cookies from 'js-cookie';
 const useSendMessage = () => {
   const [loading, setLoading] = useState(false);
   const { messages, setMessage, selectedConversation } = useConversation();
   const sendMessages = async (message) => {
     setLoading(true);
     try {
+      const token = await Cookies.get("jwt");
       const res = await axios.post(
         `https://chatapplication-ivyf.onrender.com/api/message/send/${selectedConversation._id}`,
-        { message }
+        { message },
+        {
+          credentials: "include",
+          headers: {
+              Authorization: `Bearer ${token}`,
+          },
+      }
       );
       console.log("In Use Send Message");
       setMessage([...messages, res.data.newMessage]);

7f7a11d378856429db6e7e81bd4b812e4d8af3a6
K K Harjeeth <107640043+KK-Harjeeth@users.noreply.github.com>
2024-11-24 20:46:07 +0530

    Update useGetMessage.js

--- frontend/src/context/useGetMessage.js
+++ frontend/src/context/useGetMessage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import useConversation from "../stateManage/useConversation.js";
 import axios from "axios";
+import Cookies from 'js-cookie';
 const useGetMessage = () => {
   const [loading, setLoading] = useState(false);
   const { messages, setMessage, selectedConversation } = useConversation();
@@ -10,8 +11,14 @@ const useGetMessage = () => {
       setLoading(true);
       if (selectedConversation && selectedConversation._id) {
         try {
+          const token = await Cookies.get("jwt");
           const res = await axios.get(
-            `https://chatapplication-ivyf.onrender.com/api/message/get/${selectedConversation._id}`
+            `https://chatapplication-ivyf.onrender.com/api/message/get/${selectedConversation._id}`,{
+              credentials: "include",
+              headers: {
+                  Authorization: `Bearer ${token}`,
+              },
+          }
           );
           console.log("In use Get Mesasge");
           setMessage(res.data.messages);

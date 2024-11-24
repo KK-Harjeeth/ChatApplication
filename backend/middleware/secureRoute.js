b7d86946ce7df12188f34fbb47d642d6bd04fccb
K K Harjeeth <107640043+KK-Harjeeth@users.noreply.github.com>
2024-11-24 20:30:28 +0530

    Update secureRoute.js

--- backend/middleware/secureRoute.js
+++ backend/middleware/secureRoute.js
@@ -3,7 +3,8 @@ import User from '../models/user.model.js';
 
 const secureRoute = async(req,res,next) => {
   try {
-    const token = req.cookies.jwt;
+    // const token = req.cookies.jwt;
+    const token = req.headers.authorization?.split(" ")[1];
     if(!token){
         return res.status(401).json({message:"Not authorized"})
     }

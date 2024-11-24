2fc4ca97d4a4b18eac8836be03c41ba169d7d105
K K Harjeeth <107640043+KK-Harjeeth@users.noreply.github.com>
2024-11-24 20:14:41 +0530

    Update AuthProvider.jsx

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -5,7 +5,8 @@ export default function AuthProvider({children}) {
     const initialUserState = Cookies.get("jwt");
     console.log(initialUserState);
     // parse the user data and storing in the state
-    const [authUser, setAuthUser] = useState(initialUserState?JSON.parse(initialUserState):undefined);
+    // const [authUser, setAuthUser] = useState(initialUserState?JSON.parse(initialUserState):undefined);
+    const [authUser, setAuthUser] = useState(undefined);
   return (
     <>
       <AuthContext.Provider value={{authUser,setAuthUser}}>
